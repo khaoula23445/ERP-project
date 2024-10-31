6291756da6074f1ec8854f165415f00bb53d56bd
khaoula <khaoulaslougui@gmil.com>
2024-11-01 00:19:08 +0100

    last update for today

--- pages/models.py
+++ pages/models.py
@@ -2,7 +2,7 @@
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
 #   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
@@ -35,6 +35,72 @@ class Articles(models.Model):
     class Meta:
         managed = False
         db_table = 'articles'
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
 
 
 class BankAccount(models.Model):
@@ -130,6 +196,50 @@ class Contrat(models.Model):
     class Meta:
         managed = False
         db_table = 'contrat'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
 
 
 class Employee(models.Model):
@@ -230,7 +340,7 @@ class Lots(models.Model):
 
 
 class Manager(models.Model):
-    idmanager = models.ForeignKey(Employee, models.DO_NOTHING, db_column='idmanager', primary_key=True)
+    idmanager = models.OneToOneField(Employee, models.DO_NOTHING, db_column='idmanager', primary_key=True)
 
     class Meta:
         managed = False
@@ -238,7 +348,7 @@ class Manager(models.Model):
 
 
 class Owner(models.Model):
-    idowner = models.ForeignKey('Users', models.DO_NOTHING, db_column='idowner', primary_key=True)
+    idowner = models.OneToOneField('Users', models.DO_NOTHING, db_column='idowner', primary_key=True)
 
     class Meta:
         managed = False
@@ -309,7 +419,7 @@ class ProjectTracking(models.Model):
 
 
 class ReviewEmployer(models.Model):
-    idreview_employer = models.ForeignKey(Employee, models.DO_NOTHING, db_column='idreview_employer', primary_key=True)
+    idreview_employer = models.OneToOneField(Employee, models.DO_NOTHING, db_column='idreview_employer', primary_key=True)
     employee_id = models.IntegerField(blank=True, null=True)
     review_date = models.DateField(blank=True, null=True)
     rating = models.CharField(max_length=45, blank=True, null=True)
@@ -333,7 +443,7 @@ class Salary(models.Model):
 
 
 class Secretary(models.Model):
-    idsecretary = models.ForeignKey(Employee, models.DO_NOTHING, db_column='idsecretary', primary_key=True)
+    idsecretary = models.OneToOneField(Employee, models.DO_NOTHING, db_column='idsecretary', primary_key=True)
 
     class Meta:
         managed = False
